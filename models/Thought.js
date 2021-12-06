e238ba54e3cd081c45edb0dcb326e26b517ff089
Kendra Staver <staverkendra@gmail.com>
2021-12-05 22:39:06 -0800

    outlined basic structures and requirements

---
+++ models/Thought.js
@@ -0,0 +1,14 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema({
+
+})
+
+const ThoughtSchema = new Schema({
+
+})
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
