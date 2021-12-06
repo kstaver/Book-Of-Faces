a0672f40038cf8715ffbeeed6b8588f7cd67aae9
Kendra Staver <staverkendra@gmail.com>
2021-12-06 00:11:51 -0800

    added function comments

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,6 +2,13 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
 
+    // Get all thoughts or posts
+    // Get a thought or post by id
+    // Create a thought or post
+    // Add a reaction
+    // Remove a reaction
+    // Update thoughts or posts
+    // Delete a thought or post
 };
 
 module.exports = thoughtController;
