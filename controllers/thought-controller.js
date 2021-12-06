03c08deb46728659eb52b17bc82d6d8a5b9c6d2e
Kendra Staver <staverkendra@gmail.com>
2021-12-06 00:53:35 -0800

    completed get all and get by id

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,8 +3,32 @@ const { Thought, User } = require('../models');
 const thoughtController = {
 
     // Get all thoughts or posts
+    getAllThoughts: (req, res) =>{
+        Thought.find({})
+        .populate({ path: 'user', select: '-__v'})
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
     // Get a thought or post by id
+    getThoughtByID:({ params }, res) => {
+        Thought.findOne({ _id: params.id })
+        .populate({ path: 'reactions', select: '-__v' })
+        .select('-__v')
+        .sort({ id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
     // Create a thought or post
+    createThought: (req, res) => {
+
+    }
     // Add a reaction
     // Remove a reaction
     // Update thoughts or posts
