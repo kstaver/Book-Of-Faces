3429682c7dda8bd5c3450f1db857e8171666faa8
Kendra Staver <staverkendra@gmail.com>
2021-12-06 00:11:45 -0800

    added function comments

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,6 +2,14 @@ const { User } = require('../models');
 
 const userController = {
 
+    // Get all users
+    // Get user by id
+    // Create user
+    // Add a friend
+    // Update user
+    // Delete a user
+    // Delete a friend
+
 };
 
 module.exports = userController;
