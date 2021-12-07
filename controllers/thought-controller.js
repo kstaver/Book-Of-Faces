c41bef00d2f570fa470d66940f60c90ec1416d86
Kendra Staver <staverkendra@gmail.com>
2021-12-06 16:33:20 -0800

    almost finished with tought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -25,25 +25,74 @@ const thoughtController = {
             res.status(500).json(err);
         })
     },
+
     // Create a thought or post
     createThought({ params, body }, res) {
-
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { username: body.username},
+                { $push: { thoughts: _id }},
+                { new: true }
+            );
+        })
+        .then(dbUserData =>{
+            if(!dbUserData){
+                res.status(404).json({ message: 'No user found with this username'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
     },
+
     // Add a reaction
     addReaction({ params, body }, res){
-
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reaction: body } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if(!dbThoughtData){
+                res.status(404).json({ message: 'No thought found with this id'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
     },
+
     // Remove a reaction
     removeReaction({ params }, res){
-
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true } 
+        )
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.json(err));
     },
+
     // Update thoughts or posts
     updateThought({ params, body }, res){
-
+        Thought.findOneAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true, runValidators: true }
+        )
+        .then(updatedThought => {
+            if(!updatedThought){
+                return res.status(404).json({ message: 'No thought found with this ID!' });
+            }
+            res.json(updatedThought);
+        })
+        .catch(err => res.json(err));
     },
+
     // Delete a thought or post
     deleteThought({ params, body }, res){
-        
+
     }
 };
 
