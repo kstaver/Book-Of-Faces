88be99bbe145e964d9968b9bf487b863f95709f4
Kendra Staver <staverkendra@gmail.com>
2021-12-06 22:29:25 -0800

    added get functions and create function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,8 +3,42 @@ const { User } = require('../models');
 const userController = {
 
     // Get all users
+    getAllUsers(req,res){
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
     // Get user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
     // Create user
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(500).json(err));
+    },
+    
     // Add a friend
     // Update user
     // Delete a user
