118cacf87db978507682e025b0e288781c62dfd8
Kendra Staver <staverkendra@gmail.com>
2021-12-11 22:47:28 -0800

    setup the route for Thoughts

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,3 +1,31 @@
 const router = require('express').Router();
 
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    removeReaction
+} = require('../../controllers/thoughts-controller');
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought)
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought)
+
+router
+    .route('/:id/reactions/:reactionId')
+    .post(removeReaction)
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction)
+
 module.exports = router;
