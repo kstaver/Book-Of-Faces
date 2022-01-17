eb4f9c3636f6cd8229e0de792a2cca14aa42acb8
kstaver <staverkendra@gmail.com>
2022-01-16 21:27:17 -0800

    added null check

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -119,9 +119,16 @@ const userController = {
             { _id: params.id },
             { $pull: { friends: params.friendId }},
             { new: true }
-        )
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => res.json(err));
+        ).then((dbUserData)=>{
+            if (!dbUserData) {
+                return res.status(404).json({ message: 'No user with this id!' });
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);    
+            res.json(err);
+        });
     }
 };
 
